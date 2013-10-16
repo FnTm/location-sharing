a9272eb8e55e9f258830abbc2be47f35429af70d
Klavs Taube <klavs.taube@gmail.com>
2013-10-16 21:09:46 +0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FindMyFriends::Application.config.secret_key_base = 'f80c4aa1ff7a3d144e213aa5a9ba8c7e12c53a1c8143ec7c7cb59663b0b331790b72429c20441d105223c30600c36c4954bc1c65e3d09edf175fa57a24974e64'
