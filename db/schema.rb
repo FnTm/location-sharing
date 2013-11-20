ac03cc3cd68f87fcd2cc8d12779c43e5669df9ba
Klavs Taube <klavs.taube@gmail.com>
2013-11-20 14:19:11 +0200

    Removed password_hash and salt from users.

--- db/schema.rb
+++ db/schema.rb
@@ -26,8 +26,6 @@ ActiveRecord::Schema.define(version: 20131120113213) do
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password_hash"
-    t.string   "password_salt"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.float    "latitude"
