34f445cb21169d86ed5ceae9186054f75acd17de
Klavs Taube <klavs.taube@gmail.com>
2013-11-20 14:31:36 +0200

    deleted more old authentication stuff.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,20 +6,4 @@ class User < ActiveRecord::Base
 
   has_many :user_friendships
   has_many :friends, through: :user_friendships
-  def self.authenticate(email, password)
-    user = find_by_email(email)
-    if user && user.password_hash ==
-        BCrypt::Engine.hash_secret(password, user.password_salt)
-      return user
-    else
-      return nil
-    end
-  end
-
-  def encrypt_password
-    if password.present?
-      self.password_salt = BCrypt::Engine.generate_salt
-      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
-    end
-  end
 end
