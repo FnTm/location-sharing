60630358f0c3cbc5794bd6f52ee2c3f554934a51
Klavs Taube <klavs.taube@gmail.com>
2013-11-20 13:40:31 +0200

    cleanup of silly old code.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,21 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  helper_method :current_user
-  helper_method :confirm_logged_in
-  protected
-
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  def confirm_logged_in
-    unless current_user
-      redirect_to log_in_url, :notice => "Please log in."
-      return false
-    else
-      return true
-    end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 FindMyFriends::Application.routes.draw do
 
   devise_for :users
-  get "log_out" => "sessions#destroy", :as => "log_out"
-  get "log_in" => "sessions#new", :as => "log_in"
-  get "sign_up" => "users#new", :as => "sign_up"
-  get "create_user_friendship" => "user_friendships#create", :as => "create_user_friendship"
-  root :to => "users#index"
-
-  resources :users
-  resources :sessions
-  resources :user_friendships
+  root :to => "home#index"
 end
