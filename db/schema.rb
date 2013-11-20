3b54ca51e759dd8e1e5485bc99374dd7036b955c
Klavs Taube <klavs.taube@gmail.com>
2013-11-20 13:41:52 +0200

    more devise stuff.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131120110359) do
+ActiveRecord::Schema.define(version: 20131120113213) do
 
   create_table "user_friendships", force: true do |t|
     t.integer  "user_id"
@@ -35,11 +35,12 @@ ActiveRecord::Schema.define(version: 20131120110359) do
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0, null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "encrypted_password",     default: "", null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
