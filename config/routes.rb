cd4804af9d79cfb4a0cb17d02fa14b95330b17cf
Klavs Taube <klavs.taube@gmail.com>
2014-01-15 11:19:02 +0200

    Now using custom routing (instead of Devise routing).

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 FindMyFriends::Application.routes.draw do
   devise_for :users
 
-  devise_scope :user do
-    root to: "devise/sessions#new"
-  end
-
   namespace :api, :defaults => {:format => :json} do
     namespace :v1 do
-      devise_for :users
+      as :user do
+        post '/users/', to: 'registrations#create'
+        delete '/users/:id', to: 'registrations#destroy'
+        put '/users/:id', to: 'registrations#update'
+        get '/users/:id', to: 'registrations#show'
+        put '/users/:id/location', to: 'registrations#update_location'
+      end
     end
   end
 end
