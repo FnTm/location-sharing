de9a86e0957c25877b446189a13269123f55abc8
Klavs Taube <klavs.taube@gmail.com>
2014-01-15 11:10:17 +0200

    Registrations controller now responds only to json.

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,6 +1,8 @@
 class API::V1::RegistrationsController < Devise::RegistrationsController
   include ApplicationHelper
 
+  respond_to :json
+
   skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
   skip_before_filter :authenticate_scope!, :only => [:update, :destroy]
 
